b57121fb894508c4753056ec366639cfd02717e1
Adamlin0708 <kuso12610@gmail.com>
2018-01-14 17:30:20 +0800

    S14 create website backend

--- S14 Restaurant_Forum/restaurant_forum/config/routes.rb
+++ S14 Restaurant_Forum/restaurant_forum/config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+
+  #normal user
+  # resources :restaurants, only: [:index, :show]
   root "restaurants#index"
+
+
+  #admin
+  namespace :admin do
+
+    # resources :restaurants
+    root "restaurants#index"
+
+  end
 end
